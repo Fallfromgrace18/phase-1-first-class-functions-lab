aa5d19ad968af5ec91f4fc90758f9ffcd8ef44cd
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-04-10 18:33:34 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const driverArray=['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers=function([driverArray]){
+    return['Antonia','Nuru']
+}
+const returnLastTwoDrivers=function([driverAray]){
+    return['Amari','Mo']
+}
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+function createFareMultiplier(multipler){
+    return function (fare){ 
+        return multipler*fare
+    }
+}
+const fareDoubler= createFareMultiplier(2)
+const fareTripler=createFareMultiplier(3)
+
+function selectDifferentDrivers(arrayDrivers, driverFunction){
+return driverFunction(arrayDrivers)
+}
+
+ 
